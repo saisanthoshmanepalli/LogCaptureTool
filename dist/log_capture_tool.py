275f63d8ffbf86c3ea7e45d8042606838ed4e2b8
manepalli venkata uma  saisanthosh <79530237+saisanthoshmanepalli@users.noreply.github.com>
2025-09-04 18:07:24 +0530

    final2

--- dist/log_capture_tool.py
+++ dist/log_capture_tool.py
@@ -243,7 +243,7 @@ import hashlib
 import requests  # pip install requests
 
 # ---------------- OTA CONFIG ----------------
-APP_VERSION = "1.0.0"  # bump this when you release a new build
+APP_VERSION = "1.4.4"  # bump this when you release a new build
 MANIFEST_URL = "https://raw.githubusercontent.com/saisanthoshmanepalli/LogCaptureTool/main/release/manifest.json"
 
 def get_manifest():
